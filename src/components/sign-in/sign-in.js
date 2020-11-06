532608a8a11e946b7f4c49fbef18ff31b42cb449
Safarov <elkhan.safarov10@yandex.com>
2020-11-06 15:10:33 +0500

    аутентификация Google

--- src/components/sign-in/sign-in.js
+++ src/components/sign-in/sign-in.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { useCollectionData } from 'react-firebase-hooks/firestore'
 
 function SignIn() {
+    const signInWithGoogle = () => {
+        const provider = new firebase.auth.GoogleAuthProvider()
+        auth.signInWithPopup(provider)
+    }
     return (
-        <div>
-            
-        </div>
+        <button onClick={signInWithGoogle}>Sign in with Google</button>
     )
 }
 
